3a0347c5405f15c70b99c577293e1e508c16cfa9
Gianni Bedini <giannibedini@Giannis-MacBook-Air-2.local>
2025-05-25 18:09:24 +0200

    Sostituite variabili ENV con URL-string

--- app/models.py
+++ app/models.py
@@ -2,13 +2,7 @@ import psycopg2
 import os
 
 def get_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
 
 from flask_login import UserMixin
 
